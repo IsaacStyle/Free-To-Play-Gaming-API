0afdc2b261674af934837df076361dbf34a30c6c
IsaacStyle <isaac.opra@gmail.com>
2023-01-13 11:32:10 -0500

    ok apparently adding cors is complicated

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ let app = express()
 app.use(express.json())
 app.use('/', routes)
 app.use(cors())
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    next()
+})
 
 connection.on("connected", () => {
     console.clear()
