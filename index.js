006692ceb124fdb52fe6a7bfc3405b818b57282e
IsaacStyle <isaac.opra@gmail.com>
2023-01-13 11:11:56 -0500

    add cors, soooo complicated

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ import connection from "./connection.js"
 import Games from "./models/Games.js"
 import express from "express"
 import routes from "./Routes/router.js"
+import cors from 'cors'
 
 const PORT = process.env.PORT || 3000
 let app = express()
 
 app.use(express.json())
 app.use('/', routes)
+app.use(cors())
 
 connection.on("connected", () => {
     console.clear()
